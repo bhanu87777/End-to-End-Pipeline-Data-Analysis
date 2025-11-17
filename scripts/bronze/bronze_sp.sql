8b771a5d02a9a347eb4cad9e00f53ac129c4959d
Bhanu Prakash <bhanump777@example.com>
2025-11-17 08:46:50 +0530

    Initial commit

--- scripts/bronze/bronze_sp.sql
+++ scripts/bronze/bronze_sp.sql
@@ -1,1 +1,5 @@
 EXEC bronze.load_bronze
+
+select * from bronze.crm_cust_info;
+select * from bronze.crm_prd_info;
+select * from bronze.crm_sales_details;
--- scripts/silver/silver_sp.sql
+++ scripts/silver/silver_sp.sql
@@ -1,1 +1,5 @@
 EXEC silver.load_silver
+
+select * from silver.crm_cust_info;
+select * from silver.crm_prd_info;
+select * from silver.crm_sales_details;
